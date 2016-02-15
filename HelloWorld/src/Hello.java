2066d69268f927682501a341d912f8ca92566415
Project360 <project360uw@gmail.com>
2016-02-14 18:55:18 -0800

    second change

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("HELLO WORLD!");
+		System.out.println("THIS IS THE SECOND CHANGES.");//TEST GITHUB COMMIT
+
 
 	}
 
