4bcf6b98df6a2ab64928eee37e69ad7886f19cac
Project360 <project360uw@gmail.com>
2016-02-14 19:36:25 -0800

    Added a third output.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("HELLO WORLD!");
 		System.out.println("THIS IS THE SECOND CHANGES.");//TEST GITHUB COMMIT
+		System.out.println("THIS IS THE THIRD CHANGES.");//TEST GITHUB COMMIT
 
 
 	}
